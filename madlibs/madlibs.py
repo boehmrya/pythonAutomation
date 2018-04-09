ed0d5b918f3b62e40f1f1eded05eeec7e54f7002
Ryan Boehm <ryan.w.boehm@gmail.com>
2018-04-08 20:34:01 -0400

    simple madlibs program

---
+++ madlibs/madlibs.py
@@ -0,0 +1,28 @@
+
+
+# open a files
+f = open('sample.txt')
+contents = f.read()
+words = contents.split()
+
+
+count = 0
+for w in words:
+    if w == 'ADJECTIVE':
+        newWord = input("Enter an adjective: ")
+        words[count] = newWord
+    elif w == 'ADVERB':
+        newWord = input("Enter an adverb: ")
+        words[count] = newWord
+    elif w == 'VERB':
+        newWord = input("Enter an verb: ")
+        words[count] = newWord
+    elif w == 'NOUN':
+        newWord = input("Enter an noun: ")
+        words[count] = newWord
+    count += 1
+
+
+newContents = ' '.join(words)
+out = open("out.txt","w")
+out.write(newContents)
