7607564d53d8dda14f6426bcde4b3e1eb00b8086
Ryan Boehm <ryan.w.boehm@gmail.com>
2019-03-18 22:58:52 -0400

    started script to convert euro dates to american dates in filenames

---
+++ ch9/renameDates2.py
@@ -0,0 +1,39 @@
+
+import shutil, os, re
+
+'''
+Change European-style dates to American-style dates in file names.
+'''
+
+# create a regex that matches files with the American date format
+datePattern = re.compile(r"""^(.*?))    # all text before the date
+    ((0|1)?\d)-         # one or two digits for the month
+    ((0|1|2|3)?\d)-     # one or two digits for the day
+    ((19|20)\d\d)       # four digits for the year
+    (.*?)$                # all text after the date
+    """, re.VERBOSE)
+
+for euroFilename in os.listdir('.'):
+    mo = datePattern.search(amerFilename)
+
+    if mo == None:
+        continue
+
+    beforePart  = mo.group(1)
+    monthPart   = mo.group(2)
+    dayPart     = mo.group(4)
+    yearPart    = mo.group(6)
+    afterPart   = mo.group(8)
+
+    # Form the European-style filename.
+    euroFilename = beforePart + dayPart + '-' + monthPart + '-' +
+                    yearPart + afterPart
+
+    # Get the full, absolute file paths.
+    absWorkingDir = os.path.abspath('.')
+    amerFilename = os.path.join(absWorkingDir, amerFilename)
+    euroFilename = os.path.join(absWorkingDir, euroFilename)
+
+    # Rename the files.
+    print('Renaming "%s" to "%s"...' % (amerFilename, euroFilename))
+    shutil.move(amerFilename, euroFilename)
