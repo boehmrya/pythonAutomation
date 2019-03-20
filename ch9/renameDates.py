46135d26f3323fb8eabc1717cebcb98bac9db4a5
Ryan Boehm <ryan.w.boehm@gmail.com>
2019-03-20 00:29:31 -0400

    completed program to convert euro to american dates in filenames

--- ch9/renameDates.py
+++ ch9/renameDates.py
@@ -6,14 +6,14 @@ Change American-style dates to european style dates in file names.
 '''
 
 # create a regex that matches files with the American date format
-datePattern = re.compile(r"""^(.*?))    # all text before the date
+datePattern = re.compile(r"""^(.*?)   # all text before the date
     ((0|1)?\d)-         # one or two digits for the month
     ((0|1|2|3)?\d)-     # one or two digits for the day
     ((19|20)\d\d)       # four digits for the year
     (.*?)$                # all text after the date
     """, re.VERBOSE)
 
-for amerFilename in os.listdire('.'):
+for amerFilename in os.listdir('.'):
     mo = datePattern.search(amerFilename)
 
     if mo == None:
@@ -30,7 +30,7 @@ for amerFilename in os.listdire('.'):
                     yearPart + afterPart
 
     # Get the full, absolute file paths.
-    absWorkingDir = os.path.abspath('.')
+    absWorkingDir = os.path.abspath('sample2')
     amerFilename = os.path.join(absWorkingDir, amerFilename)
     euroFilename = os.path.join(absWorkingDir, euroFilename)
 
--- ch9/renameDates2.py
+++ ch9/renameDates2.py
@@ -6,34 +6,33 @@ Change European-style dates to American-style dates in file names.
 '''
 
 # create a regex that matches files with the American date format
-datePattern = re.compile(r"""^(.*?))    # all text before the date
-    ((0|1)?\d)-         # one or two digits for the month
+datePattern = re.compile(r"""^(.*?)    # all text before the date
     ((0|1|2|3)?\d)-     # one or two digits for the day
+    ((0|1)?\d)-         # one or two digits for the month
     ((19|20)\d\d)       # four digits for the year
     (.*?)$                # all text after the date
     """, re.VERBOSE)
 
-for euroFilename in os.listdir('.'):
-    mo = datePattern.search(amerFilename)
+for euroFilename in os.listdir('sample2'):
+    mo = datePattern.search(euroFilename)
 
     if mo == None:
         continue
 
     beforePart  = mo.group(1)
-    monthPart   = mo.group(2)
-    dayPart     = mo.group(4)
+    dayPart   = mo.group(2)
+    monthPart     = mo.group(4)
     yearPart    = mo.group(6)
     afterPart   = mo.group(8)
 
     # Form the European-style filename.
-    euroFilename = beforePart + dayPart + '-' + monthPart + '-' +
-                    yearPart + afterPart
+    amerFilename = beforePart + monthPart + '-' + dayPart + '-' + yearPart + afterPart
 
     # Get the full, absolute file paths.
-    absWorkingDir = os.path.abspath('.')
-    amerFilename = os.path.join(absWorkingDir, amerFilename)
+    absWorkingDir = os.path.abspath('sample2')
     euroFilename = os.path.join(absWorkingDir, euroFilename)
+    amerFilename = os.path.join(absWorkingDir, amerFilename)
 
     # Rename the files.
-    print('Renaming "%s" to "%s"...' % (amerFilename, euroFilename))
-    shutil.move(amerFilename, euroFilename)
+    print('Renaming "%s" to "%s"...' % (euroFilename, amerFilename))
+    shutil.move(euroFilename, amerFilename)
